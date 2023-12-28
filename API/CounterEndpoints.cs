dd8320420e7b6a3975efd66b18597edd1913e27a
two-thirty-seven <83845920+two-thirty-seven@users.noreply.github.com>
2023-12-28 14:55:49 +1300

    A few tidy ups

---
+++ API/CounterEndpoints.cs
@@ -0,0 +1,21 @@
+using BlazorHtmxDemo.Components.Partials;
+using Microsoft.AspNetCore.Http.HttpResults;
+
+public static class CounterEndpoints
+{
+    public static WebApplication MapCounterEndpoints(this WebApplication app)
+    {
+        app.MapGet("/api/counter", () => new RazorComponentResult<CurrentCount>());
+        
+        app.MapPost("/api/counter", (HttpContext context) => {
+            int currentCount = context.Session.GetInt32("CurrentCount").Value;
+            currentCount++;
+            context.Session.SetInt32("CurrentCount", currentCount);
+            context.Response.Headers.Append("HX-Trigger", "counter-updated");
+
+            return new RazorComponentResult<CurrentCount>();
+        });
+
+        return app;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -14,11 +14,8 @@ builder.Services.AddSession(options =>
 });
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
-builder.Services.AddControllers();
 
 var app = builder.Build();
-
-
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -36,23 +33,8 @@ app.UseSession();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
-app.MapControllers();
 app.MapGet("/api/love-htmx", () => new RazorComponentResult<LoveHtmx>());
 app.MapGet("/api/weather", () => new RazorComponentResult<WeatherTable>());
-// app.MapGet("/api/counter", () => new RazorComponentResult<CurrentCount>());
-// app.MapPost("/api/counter/increment", (HttpContext context) => {
-//     int? currentCount = context.Session.GetInt32("CurrentCount");
-//     if (currentCount.HasValue)
-//     {
-//         currentCount++;
-//     }
-//     else {
-//        currentCount = 0; 
-//     }
-//     context.Session.SetInt32("CurrentCount", currentCount.Value);
-//     context.Response.Headers.Add("HX-Trigger", "counter-updated");
-
-//     return Results.Ok();
-// });
+app.MapCounterEndpoints();
 
 app.Run();
