8c4dc3460e73a08704576b8ae2b598761a6129e1
two-thirty-seven <83845920+two-thirty-seven@users.noreply.github.com>
2024-01-01 16:18:19 +1300

    Moves weather logic to endpoints

--- Features/Weather/WeatherEndpoints.cs
+++ Features/Weather/WeatherEndpoints.cs
@@ -5,7 +5,21 @@ public static class WeatherEndpoints
 {
     public static WebApplication MapWeatherEndpoints(this WebApplication app)
     {
-        app.MapGet("/api/weather", () => new RazorComponentResult<WeatherTable>());
+        app.MapGet("/weather/forecasts", async () => {
+            // Simulate asynchronous loading
+            await Task.Delay(500);
+
+            var startDate = DateOnly.FromDateTime(DateTime.Now);
+            var summaries = new[] { "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching" };
+            IEnumerable<WeatherForecast> forecasts = Enumerable.Range(1, 10).Select(index => new WeatherForecast
+            {
+                Date = startDate.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = summaries[Random.Shared.Next(summaries.Length)]
+            }).ToArray();
+
+            return new RazorComponentResult<WeatherTable>(new { Forecasts = forecasts });
+        });
 
         return app;
     }
