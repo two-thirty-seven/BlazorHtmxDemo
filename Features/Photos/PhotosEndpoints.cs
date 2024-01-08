7c4f4e66a065589595224bdfa96a2160dd4aef35
two-thirty-seven <83845920+two-thirty-seven@users.noreply.github.com>
2024-01-08 16:55:00 +1300

    Different approach for photos using triggers

--- Features/Photos/PhotosEndpoints.cs
+++ Features/Photos/PhotosEndpoints.cs
@@ -5,9 +5,32 @@ public static class PhotosEndpoints
 {
     public static WebApplication MapPhotosEndpoints(this WebApplication app)
     {
-        app.MapGet("/photos/search", async (int page, int size, PhotosService photosService) => {
-            var photos = await photosService.FetchPhotos(page, size);
-            return new RazorComponentResult<PhotoSheet>(new { Photos = photos });
+        app.MapGet("/photos/sheet", () => {
+            return new RazorComponentResult<PhotoSheet>();
+        });
+
+        app.MapGet("/photos/search", (int page, int size, HttpContext context) => {
+            var state = context.Session.GetObjectFromJson<PhotosState>("PhotosState");
+            state = state with { Page = page, Size = size };
+            context.Session.SetObjectAsJson("PhotosState", state);
+            context.Response.Headers.Append("HX-Trigger", "photos-state-updated");
+            return Results.Ok();
+        });
+
+        app.MapGet("/photos/prev", (HttpContext context) => {
+            var state = context.Session.GetObjectFromJson<PhotosState>("PhotosState");
+            state = state with { Page = state.Page - 1 };
+            context.Session.SetObjectAsJson("PhotosState", state);
+            context.Response.Headers.Append("HX-Trigger", "photos-state-updated");
+            return Results.Ok();
+        });
+
+        app.MapGet("/photos/next", (HttpContext context) => {
+            var state = context.Session.GetObjectFromJson<PhotosState>("PhotosState");
+            state = state with { Page = state.Page + 1 };
+            context.Session.SetObjectAsJson("PhotosState", state);
+            context.Response.Headers.Append("HX-Trigger", "photos-state-updated");
+            return Results.Ok();
         });
 
         return app;
