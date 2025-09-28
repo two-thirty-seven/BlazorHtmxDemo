23b8297f73fc9860d4ac92516117e3274a5a3674
two-thirty-seven <83845920+two-thirty-seven@users.noreply.github.com>
2025-09-29 00:03:52 +1300

    Add HXRequestOnly attribute and endpoint extensions for request validation

---
+++ Extensions/ISessionExtensions.cs
@@ -0,0 +1,18 @@
+using System.Text.Json;
+
+namespace BlazorHtmxDemo.Extensions;
+
+public static class ISessionExtensions
+{
+    public static void SetObjectAsJson<T>(this ISession session, string key, object value)
+    {
+        // Use the friendly type name as the key
+        session.SetString(key, JsonSerializer.Serialize(value));
+    }
+
+    public static T GetObjectFromJson<T>(this ISession session, string key)
+    {
+        var value = session.GetString(key);
+        return (value == null ? Activator.CreateInstance<T>() : JsonSerializer.Deserialize<T>(value))!;
+    }
+}
